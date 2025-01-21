57f9a1ddbbc93db831b2b2996e1b4b43c881a69c
BelenDevZ <florenciabelen.barbas.bogado@zabalburu.org>
2025-01-22 00:01:34 +0100

    correcciones eventosserv

--- GestorEventos/src/main/java/org/zabalburu/daw1/gestoreventos/servicio/EventosServicio.java
+++ GestorEventos/src/main/java/org/zabalburu/daw1/gestoreventos/servicio/EventosServicio.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.zabalburu.daw1.gestoreventos.servicio;
 
-/**
- *
- * @author belensexpeare
- */
+import java.util.List;
+import org.zabalburu.daw1.gestoreventos.dao.EventosDAO;
+import org.zabalburu.daw1.gestoreventos.dao.EventosList;
+import org.zabalburu.daw1.gestoreventos.modelo.Evento;
+import org.zabalburu.daw1.gestoreventos.modelo.Persona;
+
 public class EventosServicio {
+
+    private EventosDAO dao = new EventosList();
     
+    public void nuevoEvento(Evento nuevo){
+        dao.nuevoEvento(nuevo);
+    }
+
+    public Evento getEvento(int idEvento){
+        return dao.getEvento(idEvento);
+    }
+    
+    public List<Evento> getEventos(){
+        return dao.getEventos();
+    }
+    
+    public void eliminarEvento(int idEvento){
+        dao.eliminarEvento(idEvento);
+    }
+
+    public void apuntarPersonaAEvento(int id, int idEvento){
+        dao.apuntarPersonaAEvento(id, idEvento);
+    }
+
+    public void desapuntarPersonaDeEvento(int id, int idEvento){
+        dao.desapuntarPersonaDeEvento(id, idEvento);
+    }
+    
+    public List<Persona> getPersonasPorEvento(int idEvento) {
+        return dao.getPersonasEvento(idEvento);
+    }
 }
